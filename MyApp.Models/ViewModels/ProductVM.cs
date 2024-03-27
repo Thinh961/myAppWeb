8de76a888da72cae78f54dfe130b6756a039b983
Thinh Nguyen Chi <65598576+Thinh961@users.noreply.github.com>
2024-03-27 17:29:36 +0700

    add gitinore

---
+++ MyApp.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace MyApp.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
---
+++ MyAppWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
+
+namespace MyAppWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            ProductVM productVM = new ProductVM();
+            productVM.Products = _unitOfWork.Product.GetAll();
+            return View(productVM);
+        }
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Product Product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Add(Product);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Product created done!";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(Product);
+        //}
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new(),
+                Categories = _unitOfWork.Category.GetAll().Select(x =>
+                new System.Web.Mvc.SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                productVM.Product = _unitOfWork.Product.GetT(x => x.Id == id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(productVM);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                if(productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                    TempData["success"] = "Product created done!";
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                    TempData["success"] = "Product updated done!";
+                }
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var Product = _unitOfWork.Product.GetT(X => X.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            return View(Product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var Product = _unitOfWork.Product.GetT(X => X.Id == id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Delete(Product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted done!";
+            return RedirectToAction("Index");
+        }
+    }
+}
